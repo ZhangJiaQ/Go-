21e4f157f5af701bc0935dbc378d137874eff506
ZhangJiaQ <529326573@qq.com>
2020-03-15 06:46:58 +0000

    leetcode

--- leetcode/src/Dynamic_Programming/70. Climbing Stairs.go
+++ leetcode/src/Dynamic_Programming/70. Climbing Stairs.go
@@ -1,3 +1,21 @@
 package Dynamic_Programming
 
+func climbStairs(n int) int {
+	if n == 1 {
+		return 1
+	}
+	if n == 2{
+		return 2
+	}
+	result := make([]int, n)
+	result[0] = 1
+	result[1] = 2
+	index := 2
+	for index < n {
+		result[index] = result[index-1] + result[index-2]
+		index += 1
+	}
+	return result[n-1]
+
+}
 
